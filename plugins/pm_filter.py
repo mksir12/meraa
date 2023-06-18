6d4334916eeee260239d52f355bde3e8a88aaa24
mksir12 <116490353+mksir12@users.noreply.github.com>
2023-06-18 18:38:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1142,11 +1142,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('Bᴏᴛ Oᴡɴᴇʀ 💎', callback_data="owner_info"),
+                    InlineKeyboardButton('Bᴏᴛ Oᴡɴᴇʀ 👨🏻‍💻', callback_data="owner_info"),
                     InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ 🎬', url=GRP_LNK)
                 ],[
                     InlineKeyboardButton('Hᴇʟᴘ ♻️', callback_data='help'),
-                    InlineKeyboardButton('Aʙᴏᴜᴛ 📶', callback_data='about'),
+                    InlineKeyboardButton('Aʙᴏᴜᴛ 📚', callback_data='about'),
 
                 ],[
                     InlineKeyboardButton('™️ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ™️', url=CHNL_LNK)

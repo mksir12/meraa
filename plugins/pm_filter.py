f536f7a8dd759cf48dcf3330400aef1d7db6f444
mksir12 <116490353+mksir12@users.noreply.github.com>
2023-06-19 18:07:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1245,33 +1245,21 @@ async def cb_handler(client: Client, query: CallbackQuery):
             parse_mode=enums.ParseMode.HTML
         )
         elif query.data == "oggy":
-
-        buttons = [[
-
-            InlineKeyboardButton('⟸ Bᴀᴄᴋ', callback_data='about')
-
-        ]]
-
-        reply_markup = InlineKeyboardMarkup(buttons)
-
-        await client.edit_message_media(
-
-            query.message.chat.id, 
-
-            query.message.id, 
-
-            InputMediaPhoto(random.choice(PICS))
-
-        )
-
-        await query.message.edit_text(
-
-            text=script.OGGY_TXT,
-
-            reply_markup=reply_markup,
-
-            parse_mode=enums.ParseMode.HTML
-        )
+            buttons = [[
+                InlineKeyboardButton('back', callback_data='about')
+            ]]    
+            reply_markup = InlineKeyboardMarkup(buttons)
+            await client.edit_message_media(
+                query.message.chat.id,
+                query.message.id,
+                InputMediaPhoto(random.choice(PICS))
+            )
+            await query.message.edit_text(
+                text=script.OGGY_TXT,
+                reply_markup=reply_markup,
+                parse_mode=enums.ParseMode.HTML
+            )
+                
     elif query.data == "source":
         buttons = [[
             InlineKeyboardButton('⟸ Bᴀᴄᴋ', callback_data='about')
